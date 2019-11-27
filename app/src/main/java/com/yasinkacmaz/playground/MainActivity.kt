8ee3c2828a5ef146370f9f6e8adbe96caeb1cd2e
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2019-11-27 17:43:08 +0300

    Implement android compose

--- app/src/main/java/com/yasinkacmaz/playground/MainActivity.kt
+++ app/src/main/java/com/yasinkacmaz/playground/MainActivity.kt
@@ -2,11 +2,16 @@ package com.yasinkacmaz.playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            PlaygroundTheme {
+                Text("Hello World")
+            }
+        }
     }
 }
