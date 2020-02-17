f671998c2ef275d9068eb51f184d559d18e1ae98
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2020-02-17 16:37:22 +0300

    Implement has android injector

--- app/src/main/java/com/yasinkacmaz/playground/ui/application/PlaygroundApplication.kt
+++ app/src/main/java/com/yasinkacmaz/playground/ui/application/PlaygroundApplication.kt
@@ -1,9 +1,26 @@
 package com.yasinkacmaz.playground.ui.application
 
 import android.app.Application
+import com.yasinkacmaz.playground.util.unsafeLazy
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class PlaygroundApplication : Application() {
+class PlaygroundApplication : Application(), HasAndroidInjector {
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    private val applicationComponent: ApplicationComponent by unsafeLazy(::buildComponent)
+
     override fun onCreate() {
         super.onCreate()
+        applicationComponent.inject(this)
     }
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
+
+    private fun buildComponent() = DaggerApplicationComponent.builder()
+        .application(this)
+        .build()
 }
