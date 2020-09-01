b7f5a9eee775557122e540a47189b9b4d91c3178
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2020-09-01 19:05:10 +0300

    Fix movie list ui

--- app/src/main/java/com/yasinkacmaz/playground/ui/main/MainActivity.kt
+++ app/src/main/java/com/yasinkacmaz/playground/ui/main/MainActivity.kt
@@ -6,12 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.InnerPadding
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.preferredSize
-import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
@@ -22,7 +20,6 @@ import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.yasinkacmaz.playground.R
-import com.yasinkacmaz.playground.data.Movie
 import com.yasinkacmaz.playground.ui.theme.PlayGroundTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -44,30 +41,30 @@ class MainActivity : AppCompatActivity() {
             PlayGroundTheme {
                 Scaffold(
                     topBar = {
-                        topAppBar()
+                        TopAppBar()
                     },
                     bottomBar = {
-                        bottomNavigation()
+                        BottomNavigation()
                     },
                     bodyContent = {
-                        bodyContent()
+                        BodyContent()
                     })
             }
         }
     }
 
     @Composable
-    private fun bodyContent() {
+    private fun BodyContent() {
         val uiState = mainViewModel.uiState.collectAsState().value
         if (uiState.loading) {
-            loading()
+            Loading()
         } else {
-            movies(uiState.movies)
+            Movies(uiState.movies)
         }
     }
 
     @Composable
-    private fun loading() {
+    private fun Loading() {
         Column(
             modifier = Modifier.fillMaxWidth().fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
@@ -75,13 +72,6 @@ class MainActivity : AppCompatActivity() {
         ) {
             Text(text = stringResource(id = R.string.fetching_movies))
             CircularProgressIndicator(modifier = Modifier.preferredSize(40.dp).padding(top = 16.dp))
-        }
-    }
-
-    @Composable
-    private fun movies(movies: List<Movie>) {
-        LazyColumnFor(contentPadding = InnerPadding(all = 16.dp), items = movies) { movie ->
-            MovieLayout(movie)
         }
     }
 }
