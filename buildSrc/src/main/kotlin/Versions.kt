6bc75b1238750a92d5934d91755d4b9cdb358088
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2020-10-06 22:17:48 +0300

    Bump version to 1.1.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
     const val minSdk = 21
     const val targetSdk = 30
     const val compileSdk = 30
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1.0"
 }
