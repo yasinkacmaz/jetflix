9f7d2e3e1ba8c68f7f5c95dc2879f5d138dd0431
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2020-10-10 22:37:47 +0300

    Use ktlint gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 buildscript {
     repositories {
         google()
         jcenter()
         maven("https://dl.bintray.com/kotlin/kotlin-eap/")
+        maven("https://plugins.gradle.org/m2/")
     }
     dependencies {
         classpath(Dependencies.Gradle.androidBuildPlugin)
@@ -20,4 +23,19 @@ allprojects {
 
 task("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+plugins {
+    id(Dependencies.Gradle.Ktlint.plugin) version (Dependencies.Gradle.Ktlint.pluginVersion)
+}
+
+ktlint {
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.HTML)
+    }
+    filter {
+        exclude("**/generated/**")
+        include("**/kotlin/**")
+    }
 }
