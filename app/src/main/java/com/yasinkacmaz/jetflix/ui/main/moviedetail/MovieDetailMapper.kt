6998abcda2ac4751c79c1695d2e2eb986521e7c5
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2020-10-10 20:25:33 +0300

    Use mapper for api to ui model transformation

--- app/src/main/java/com/yasinkacmaz/jetflix/ui/main/moviedetail/MovieDetailMapper.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/ui/main/moviedetail/MovieDetailMapper.kt
@@ -1,22 +1,23 @@
 package com.yasinkacmaz.jetflix.ui.main.moviedetail
 
 import com.yasinkacmaz.jetflix.data.MovieDetailResponse
+import com.yasinkacmaz.jetflix.util.Mapper
 import com.yasinkacmaz.jetflix.util.toBackdropUrl
 import com.yasinkacmaz.jetflix.util.toPosterUrl
 import javax.inject.Inject
 
-class MovieDetailMapper @Inject constructor() {
-    fun map(response: MovieDetailResponse) = MovieDetail(
-        id = response.id,
-        title = response.title,
-        originalTitle = response.originalTitle,
-        overview = response.overview,
-        backdropUrl = response.backdropPath.orEmpty().toBackdropUrl(),
-        posterUrl = response.posterPath.toPosterUrl(),
-        genres = response.genres,
-        releaseDate = response.releaseDate,
-        voteAverage = response.voteAverage,
-        voteCount = response.voteCount,
-        duration = response.runtime ?: -1
+class MovieDetailMapper @Inject constructor() : Mapper<MovieDetailResponse, MovieDetail> {
+    override fun map(input: MovieDetailResponse) = MovieDetail(
+        id = input.id,
+        title = input.title,
+        originalTitle = input.originalTitle,
+        overview = input.overview,
+        backdropUrl = input.backdropPath.orEmpty().toBackdropUrl(),
+        posterUrl = input.posterPath.toPosterUrl(),
+        genres = input.genres,
+        releaseDate = input.releaseDate,
+        voteAverage = input.voteAverage,
+        voteCount = input.voteCount,
+        duration = input.runtime ?: -1
     )
 }
