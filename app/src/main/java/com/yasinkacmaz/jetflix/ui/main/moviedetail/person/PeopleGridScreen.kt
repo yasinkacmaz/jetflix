7d0b4fcd93c54b74a025a30aa462e882da1d8c7a
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-02-14 04:21:06 +0300

    Increase stability of people grid screen animation
    Animate person based on scroll direction
    Animate person by calculating its row and column
    Add easing based on scroll direction
    Add delay based on item position and scroll direction
    Calculate current visible items

--- app/src/main/java/com/yasinkacmaz/jetflix/ui/main/moviedetail/person/PeopleGridScreen.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/ui/main/moviedetail/person/PeopleGridScreen.kt
@@ -1,6 +1,10 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.yasinkacmaz.jetflix.ui.main.moviedetail.person
 
+import androidx.compose.animation.core.Easing
 import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
@@ -10,11 +14,15 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyGridScope
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.yasinkacmaz.jetflix.ui.main.moviedetail.credits.Person
 import com.yasinkacmaz.jetflix.util.animation.ScaleAndAlphaArgs
@@ -22,7 +30,6 @@ import com.yasinkacmaz.jetflix.util.animation.scaleAndAlpha
 import com.yasinkacmaz.jetflix.util.toDp
 import dev.chrisbanes.accompanist.insets.LocalWindowInsets
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PeopleGridScreen(people: List<Person>) {
     val insets = LocalWindowInsets.current
@@ -35,6 +42,7 @@ fun PeopleGridScreen(people: List<Person>) {
             .fillMaxHeight()
             .background(MaterialTheme.colors.surface)
     ) {
+        val state = rememberLazyListState()
         LazyVerticalGrid(
             cells = GridCells.Fixed(columnCount),
             contentPadding = PaddingValues(
@@ -43,30 +51,50 @@ fun PeopleGridScreen(people: List<Person>) {
                 top = statusBarPadding,
                 bottom = navigationBarPadding
             ),
-            content = {
-                items(people.count()) { index ->
-                    val person = people[index]
-                    BoxWithConstraints(Modifier.padding(horizontal = horizontalPadding, vertical = 8.dp)) {
-                        // TODO: Can we access visible item count to determine index more accurately for animation.
-                        //  Lets say we have 500 items but only 10 of them is visible to user.
-                        //  I want to get the index on the screen, not the index in the whole dataset.
-                        val animation = tween<Float>(
-                            durationMillis = 600,
-                            delayMillis = 300 * (index % columnCount),
-                            easing = FastOutSlowInEasing
-                        )
-                        val args = ScaleAndAlphaArgs(fromScale = 2f, toScale = 1f, fromAlpha = 0f, toAlpha = 1f)
-                        val (scale, alpha) = scaleAndAlpha(args = args, animation = animation)
-                        Person(
-                            profilePhotoUrl = person.profilePhotoUrl,
-                            name = person.name,
-                            job = person.character,
-                            gender = person.gender,
-                            modifier = Modifier.graphicsLayer(alpha = alpha, scaleX = scale, scaleY = scale)
-                        )
-                    }
-                }
-            }
+            state = state,
+            content = { peopleGridContent(people, columnCount, state, horizontalPadding) }
         )
     }
+}
+
+private fun LazyGridScope.peopleGridContent(
+    people: List<Person>,
+    columnCount: Int,
+    state: LazyListState,
+    horizontalPadding: Dp
+) {
+    items(people.count()) { index ->
+        val (delay, easing) = state.calculateDelayAndEasing(index, columnCount)
+        BoxWithConstraints(Modifier.padding(horizontal = horizontalPadding, vertical = 8.dp)) {
+            val animation = tween<Float>(durationMillis = 500, delayMillis = delay, easing = easing)
+            val args = ScaleAndAlphaArgs(fromScale = 2f, toScale = 1f, fromAlpha = 0f, toAlpha = 1f)
+            val (scale, alpha) = scaleAndAlpha(args = args, animation = animation)
+            val person = people[index]
+            Person(
+                profilePhotoUrl = person.profilePhotoUrl,
+                name = person.name,
+                job = person.character,
+                gender = person.gender,
+                modifier = Modifier.graphicsLayer(alpha = alpha, scaleX = scale, scaleY = scale)
+            )
+        }
+    }
+}
+
+@Composable
+private fun LazyListState.calculateDelayAndEasing(index: Int, columnCount: Int): Pair<Int, Easing> {
+    val row = index / columnCount
+    val column = index % columnCount
+    val firstVisibleRow = firstVisibleItemIndex + 1
+    val visibleRows = layoutInfo.visibleItemsInfo.count()
+    val scrollingToBottom = firstVisibleRow < row
+    val rowDelay = 200 * when {
+        scrollingToBottom && visibleRows != 0 -> visibleRows + firstVisibleRow - row // scrolling to bottom
+        visibleRows == 0 -> row // initial load
+        else -> 1 // scrolling to top
+    }
+    val scrollDirectionMultiplier = if (scrollingToBottom || visibleRows == 0) 1 else -1
+    val columnDelay = column * 150 * scrollDirectionMultiplier
+    val easing = if (scrollingToBottom || visibleRows == 0) LinearOutSlowInEasing else FastOutSlowInEasing
+    return rowDelay + columnDelay to easing
 }
