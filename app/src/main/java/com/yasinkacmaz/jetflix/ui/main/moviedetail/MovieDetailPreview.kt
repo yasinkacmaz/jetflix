e48bf6b9ad255f00f26bc9384e99598f3c5d9fb2
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-04-24 20:31:55 +0300

    Update movie detail preview

--- app/src/main/java/com/yasinkacmaz/jetflix/ui/main/moviedetail/MovieDetailPreview.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/ui/main/moviedetail/MovieDetailPreview.kt
@@ -4,6 +4,7 @@ import androidx.activity.OnBackPressedDispatcher
 import androidx.compose.animation.Animatable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,28 +25,32 @@ private fun MovieDetailPreview() {
         id = 1337,
         title = "A great movie",
         originalTitle = "Un grand film",
-        overview = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        """,
-        tagline = "Enjoy a great movie",
+        overview = """
+            |Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            |et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            |aliquip ex ea commodo consequat.""".trimMargin(),
+        tagline = "My first movie",
         backdropUrl = "url",
         posterUrl = "url",
-        genres = listOf(Genre(1, "Action"), Genre(2, "Comedy")),
-        releaseDate = "01.03.1337",
+        genres = listOf(Genre(1, "Action"), Genre(2, "Comedy"), Genre(3, "Fantasy")),
+        releaseDate = "06.12.1994",
         voteAverage = 5.7,
         voteCount = 1337,
         duration = 137,
-        productionCompanies = listOf(ProductionCompany("Marvel", ""), ProductionCompany("Pixar", "")),
-        homepage = ""
+        productionCompanies = listOf(
+            ProductionCompany("Marvel", ""),
+            ProductionCompany("Pixar", ""),
+            ProductionCompany("Warner Bros", "")
+        ),
+        homepage = "homepageUrl"
     )
-    val person = Person(profilePhotoUrl = "", name = "Yasin", role = "Android Developer", gender = Gender.MALE)
-    val credits = Credits(cast = listOf(person, person, person), crew = listOf(person, person, person))
+    val person = Person(profilePhotoUrl = "", name = "Yasin Kaçmaz", role = "Android Developer", gender = Gender.MALE)
+    val people = listOf(person, person, person, person)
+    val credits = Credits(cast = people, crew = people)
     val navigator = Navigator<Screen>(Screen.Movies, LocalLifecycleOwner.current, OnBackPressedDispatcher {})
+    val vibrantColor = remember { Animatable(Color.randomColor()) }
     CompositionLocalProvider(
-        LocalVibrantColor provides Animatable(Color.randomColor()),
+        LocalVibrantColor provides vibrantColor,
         LocalNavigator provides navigator
     ) {
         val images = listOf(Image("", 1), Image("", 1), Image("", 1))
