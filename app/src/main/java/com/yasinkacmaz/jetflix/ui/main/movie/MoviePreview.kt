759d81cedbbd6dd7fe59435e2e35a1467666c52e
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-04-24 22:28:39 +0300

    Use placeholder drawables at composable previews

--- app/src/main/java/com/yasinkacmaz/jetflix/ui/main/movie/MoviePreview.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/ui/main/movie/MoviePreview.kt
@@ -1,8 +1,12 @@
 package com.yasinkacmaz.jetflix.ui.main.movie
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yasinkacmaz.jetflix.ui.theme.JetflixTheme
 
 @Preview(name = "Preview", group = "Size")
@@ -18,5 +22,7 @@ private fun MovieMultiPreview() {
 
 @Composable
 fun MoviePreview(content: @Composable () -> Unit) {
-    JetflixTheme(content = content)
+    Surface(Modifier.padding(16.dp)) {
+        JetflixTheme(content = content)
+    }
 }
