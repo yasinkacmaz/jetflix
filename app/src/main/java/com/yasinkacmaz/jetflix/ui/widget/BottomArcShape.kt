424ef3beea1f043eea592ff9c9712ef9b03e5663
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-04-24 19:42:17 +0300

    Fix bottom arc shape arc height

--- app/src/main/java/com/yasinkacmaz/jetflix/ui/widget/BottomArcShape.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/ui/widget/BottomArcShape.kt
@@ -2,7 +2,10 @@ package com.yasinkacmaz.jetflix.ui.widget
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
@@ -11,23 +14,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import com.yasinkacmaz.jetflix.util.dpToPx
 
 class BottomArcShape(private val arcHeight: Float) : Shape {
     override fun createOutline(size: Size, layoutDirection: LayoutDirection, density: Density): Outline {
         val path = Path().apply {
-            moveTo(size.width, 0f)
-            lineTo(size.width, size.height)
-            val arcOffset = arcHeight / 4
+            relativeMoveTo(size.width, 0f)
+            relativeLineTo(size.width, size.height)
+            val arcOffset = arcHeight / 10
             val rect = Rect(
-                0f - arcOffset,
-                size.height - arcHeight,
-                size.width + arcOffset,
-                size.height
+                left = 0f - arcOffset,
+                top = size.height - arcHeight,
+                right = size.width + arcOffset,
+                bottom = size.height
             )
             arcTo(rect, 0f, 180f, false)
             lineTo(0f, 0f)
@@ -38,8 +41,18 @@ class BottomArcShape(private val arcHeight: Float) : Shape {
 }
 
 @Composable
-@Preview
+@Preview(showSystemUi = true, showBackground = true)
 fun BottomArcShapePreview() {
-    val shape = BottomArcShape(100.dp.value * LocalDensity.current.density)
-    Box(Modifier.size(200.dp, 300.dp).background(color = Color.Magenta, shape = shape))
+    val shape = BottomArcShape(240.dpToPx())
+    Surface(
+        Modifier
+            .wrapContentSize()
+            .padding(32.dp)
+    ) {
+        Box(
+            Modifier
+                .size(320.dp, 480.dp)
+                .background(color = Color.Magenta, shape = shape)
+        )
+    }
 }
