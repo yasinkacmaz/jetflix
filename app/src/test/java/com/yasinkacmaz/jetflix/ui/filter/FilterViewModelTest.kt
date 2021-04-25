686dc71195031aa3fa00920b57d24914e26f0f19
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-04-25 14:25:12 +0300

    Fix failing FilterViewModel test

--- app/src/test/java/com/yasinkacmaz/jetflix/ui/filter/FilterViewModelTest.kt
+++ app/src/test/java/com/yasinkacmaz/jetflix/ui/filter/FilterViewModelTest.kt
@@ -3,15 +3,16 @@ package com.yasinkacmaz.jetflix.ui.filter
 import com.yasinkacmaz.jetflix.data.Genre
 import com.yasinkacmaz.jetflix.data.GenresResponse
 import com.yasinkacmaz.jetflix.service.MovieService
-import com.yasinkacmaz.jetflix.ui.filter.option.SortBy
 import com.yasinkacmaz.jetflix.ui.filter.genres.GenreUiModel
 import com.yasinkacmaz.jetflix.ui.filter.genres.GenreUiModelMapper
+import com.yasinkacmaz.jetflix.ui.filter.option.SortBy
 import com.yasinkacmaz.jetflix.util.CoroutineTestRule
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import org.junit.Before
@@ -34,7 +35,6 @@ class FilterViewModelTest {
     @RelaxedMockK
     private lateinit var genreUiModelMapper: GenreUiModelMapper
 
-    private lateinit var filterViewModel: FilterViewModel
     private val genre = Genre(1, "Name")
     private val genreUiModel = GenreUiModel(genre = genre)
 
@@ -44,7 +44,6 @@ class FilterViewModelTest {
         coEvery { filterDataStore.filterState } returns flowOf()
         every { genreUiModelMapper.map(genre) } returns genreUiModel
         coEvery { movieService.fetchGenres() } returns GenresResponse(listOf(genre))
-        filterViewModel = FilterViewModel(filterDataStore, movieService, genreUiModelMapper)
     }
 
     @Test
@@ -52,19 +51,20 @@ class FilterViewModelTest {
         val filterState = FilterState(sortBy = SortBy.REVENUE)
         coEvery { filterDataStore.filterState } returns flowOf(filterState)
 
+        val filterViewModel = initViewModel()
+
         expectThat(filterViewModel.filterState.first()).isEqualTo(filterState.copy(genres = listOf(genreUiModel)))
-        coVerify { filterDataStore.filterState }
         coVerify { movieService.fetchGenres() }
     }
 
     @Test
     fun `Should fetch genres only once`() = coroutineTestRule.runBlockingTest {
         val filterState = FilterState(sortBy = SortBy.REVENUE)
-        coEvery { filterDataStore.filterState } returns flowOf(filterState, filterState, filterState)
+        every { filterDataStore.filterState } returns flowOf(filterState, filterState, filterState)
 
-        // We only access flow to init lazy
-        filterViewModel.filterState
+        val filterViewModel = initViewModel()
 
+        expectThat(filterViewModel.filterState.first()).isEqualTo(filterState.copy(genres = listOf(genreUiModel)))
         coVerify(exactly = 1) { movieService.fetchGenres() }
     }
 
@@ -74,20 +74,29 @@ class FilterViewModelTest {
         val filterState = FilterState(sortBy = SortBy.REVENUE)
         coEvery { filterDataStore.filterState } returns flowOf(filterState)
 
+        val filterViewModel = initViewModel()
+
         expectThat(filterViewModel.filterState.first()).isEqualTo(filterState.copy(genres = emptyList()))
     }
 
     @Test
     fun `onResetClicked should call data store resetFilterState`() = coroutineTestRule.runBlockingTest {
+        val filterState = FilterState(sortBy = SortBy.REVENUE)
+        coEvery { filterDataStore.filterState } returns flowOf(filterState)
+
+        val filterViewModel = initViewModel()
         filterViewModel.onResetClicked()
 
+        expectThat(filterViewModel.filterState.first()).isEqualTo(filterState.copy(genres = listOf(genreUiModel)))
         coVerify { filterDataStore.resetFilterState() }
     }
 
     @Test
     fun `onFilterStateChanged should call data store onFilterStateChanged`() = coroutineTestRule.runBlockingTest {
+        coEvery { filterDataStore.filterState } returns flowOf(FilterState())
         val newFilterState = FilterState(sortBy = SortBy.REVENUE)
 
+        val filterViewModel = initViewModel()
         filterViewModel.onFilterStateChanged(newFilterState)
 
         coVerify { filterDataStore.onFilterStateChanged(newFilterState) }
@@ -96,8 +105,15 @@ class FilterViewModelTest {
     @Test
     fun `filter state should change when filter data store changed`() = coroutineTestRule.runBlockingTest {
         val filterState = FilterState(sortBy = SortBy.REVENUE)
-        coEvery { filterDataStore.filterState } returns flowOf(filterState)
+        val filterStateFlow = MutableStateFlow(filterState)
+        coEvery { filterDataStore.filterState } returns filterStateFlow
+        val filterViewModel = initViewModel()
 
-        expectThat(filterViewModel.filterState.first()).isEqualTo(filterState.copy(genres = listOf(genreUiModel)))
+        val changedFilterState = FilterState(sortBy = SortBy.RELEASE_DATE)
+        filterStateFlow.emit(changedFilterState)
+        val expectedState = changedFilterState.copy(genres = listOf(genreUiModel))
+        expectThat(filterViewModel.filterState.first()).isEqualTo(expectedState)
     }
+
+    private fun initViewModel() = FilterViewModel(filterDataStore, movieService, genreUiModelMapper)
 }
