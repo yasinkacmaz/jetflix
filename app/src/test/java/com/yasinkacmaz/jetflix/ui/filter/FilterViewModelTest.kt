ab230f385d9ea6fc671f12f1f90cb3dc87758d44
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-04-25 03:01:08 +0300

    Disable fork option of java compile tasks

--- app/src/test/java/com/yasinkacmaz/jetflix/ui/filter/FilterViewModelTest.kt
+++ app/src/test/java/com/yasinkacmaz/jetflix/ui/filter/FilterViewModelTest.kt
@@ -3,9 +3,9 @@ package com.yasinkacmaz.jetflix.ui.filter
 import com.yasinkacmaz.jetflix.data.Genre
 import com.yasinkacmaz.jetflix.data.GenresResponse
 import com.yasinkacmaz.jetflix.service.MovieService
-import com.yasinkacmaz.jetflix.ui.filter.option.SortBy
 import com.yasinkacmaz.jetflix.ui.filter.genres.GenreUiModel
 import com.yasinkacmaz.jetflix.ui.filter.genres.GenreUiModelMapper
+import com.yasinkacmaz.jetflix.ui.filter.option.SortBy
 import com.yasinkacmaz.jetflix.util.CoroutineTestRule
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
@@ -14,7 +14,6 @@ import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.onEach
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -61,12 +60,10 @@ class FilterViewModelTest {
     @Test
     fun `Should fetch genres only once`() = coroutineTestRule.runBlockingTest {
         val filterState = FilterState(sortBy = SortBy.REVENUE)
-        coEvery { filterDataStore.filterState } returns flowOf(filterState, filterState, filterState)
+        every { filterDataStore.filterState } returns flowOf(filterState, filterState, filterState)
 
-        // We only collect flow to pass this test
-        filterViewModel.filterState.onEach {
-            print(it)
-        }
+        // Init lazy filter state by accessing it
+        filterViewModel.filterState
 
         coVerify(exactly = 1) { movieService.fetchGenres() }
     }
