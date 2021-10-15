a41a608b67e31939a0178ddd2d0715c8d069fd56
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-10-16 02:11:16 +0300

    Bump dagger hilt version to 2.39.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 object Dependencies {
     const val okHttp = "com.squareup.okhttp3:okhttp:5.0.0-alpha.2"
     const val ktlint = "com.pinterest:ktlint:0.41.0"
-    const val daggerHiltVersion = "2.36"
+    const val daggerHiltVersion = "2.39.1"
 
     object Kotlin {
         const val version = "1.5.31"
