3a95fc8bdc7a3a71c0691fd27a56c2b60b7f6853
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-10-16 01:51:35 +0300

    Set target sdk and compile sdk to 31

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val minSdk = 21
-    const val targetSdk = 30
-    const val compileSdk = 30
+    const val targetSdk = 31
+    const val compileSdk = 31
     const val versionCode = 2
     const val versionName = "1.1.0"
 }
