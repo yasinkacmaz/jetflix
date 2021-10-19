f686d2d573ae05943cdffc7ac29399afc680fd5b
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-10-19 23:59:11 +0300

    Bump kotlinx serialization version to 1.3.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,7 +7,7 @@ object Dependencies {
     object Kotlin {
         const val version = "1.5.31"
         const val stdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${version}"
-        const val jsonSerialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0"
+        const val jsonSerialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0"
     }
 
     object Gradle {
