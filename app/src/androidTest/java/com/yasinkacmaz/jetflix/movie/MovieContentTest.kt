8d3a65abb4679027080211fc6018f1d46b9114b8
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-10-28 19:02:28 +0300

    Fix movie content ui tests

--- app/src/androidTest/java/com/yasinkacmaz/jetflix/movie/MovieContentTest.kt
+++ app/src/androidTest/java/com/yasinkacmaz/jetflix/movie/MovieContentTest.kt
@@ -1,17 +1,12 @@
 package com.yasinkacmaz.jetflix.movie
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.printToLog
 import androidx.compose.ui.unit.dp
 import com.yasinkacmaz.jetflix.ui.movies.movie.Movie
 import com.yasinkacmaz.jetflix.ui.movies.movie.MovieContent
@@ -23,48 +18,28 @@ class MovieContentTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    private val movie = Movie(
-        id = 1337,
-        name = "Movie Name",
-        releaseDate = "01.03.1337",
-        posterPath = "",
-        voteAverage = 9.24,
-        voteCount = 1337
-    )
-
     @Test
-    fun should_not_find_movie_information_with_merged_tree() = with(composeTestRule) {
-        renderMovieContent()
+    fun should_render_basic_movie_information(): Unit = with(composeTestRule) {
+        val movie = Movie(
+            id = 1337,
+            name = "Movie Name",
+            releaseDate = "01.03.1337",
+            posterPath = "",
+            voteAverage = 9.24,
+            voteCount = 1337
+        )
 
-        onRoot().printToLog(TAG)
-        onNodeWithText(movie.name, useUnmergedTree = false).assertDoesNotExist()
-        onNodeWithText(movie.releaseDate, useUnmergedTree = false).assertDoesNotExist()
-        onNodeWithText(movie.voteCount.toString(), useUnmergedTree = false).assertDoesNotExist()
-    }
+        renderMovieContent(movie)
 
-    @Test
-    fun should_assert_movie_information_with_unmerged_tree(): Unit = with(composeTestRule) {
-        renderMovieContent()
-
-        onRoot().printToLog(TAG)
         onNodeWithText(movie.name, useUnmergedTree = true).assertIsDisplayed()
         onNodeWithText(movie.releaseDate, useUnmergedTree = true).assertIsDisplayed()
         onNodeWithText(movie.voteCount.toString(), useUnmergedTree = true).assertIsDisplayed()
+        onNodeWithText(movie.voteAverage.toString(), useUnmergedTree = true).assertIsDisplayed()
     }
 
-    private fun ComposeContentTestRule.renderMovieContent() = setTestContent {
-        Box(
-            Modifier
-                .fillMaxHeight(0.5f)
-                .fillMaxWidth()
-                .padding(48.dp),
-            contentAlignment = Alignment.Center
-        ) {
+    private fun ComposeContentTestRule.renderMovieContent(movie: Movie) = setTestContent {
+        Box(Modifier.padding(72.dp)) {
             MovieContent(movie)
         }
-    }
-
-    companion object {
-        private const val TAG = "MovieContentTest"
     }
 }
