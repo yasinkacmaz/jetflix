18c0f8a8d057da2a7dee77aeb7bb5dabbfee54be
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-11-12 15:04:13 +0300

    Fix language dropdown menu ui
    Set language dropdown menu height as fixed
    Remove resize transformation from flag images
    Increase flag and tick icon inline content size

--- app/src/main/java/com/yasinkacmaz/jetflix/ui/settings/SettingsContent.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/ui/settings/SettingsContent.kt
@@ -5,9 +5,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.foundation.text.appendInlineContent
@@ -20,19 +20,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.FixedScale
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
@@ -46,17 +44,18 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
 import com.yasinkacmaz.jetflix.R
 import com.yasinkacmaz.jetflix.ui.common.loading.LoadingRow
-import com.yasinkacmaz.jetflix.util.transformation.SizeTransformation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 const val SETTINGS_DIALOG_TAG = "SettingsDialog"
 private const val FLAG_ID = "flag"
 private const val TICK_ID = "tickIcon"
 private const val DROPDOWN_ID = "dropdownIcon"
-private const val RESIZE_PERCENT = 25
-private val LocalSizeTransformation = staticCompositionLocalOf<SizeTransformation> {
-    error("No transformation instance available")
-}
+
+private val placeholder = Placeholder(
+    width = 2.5.em,
+    height = 1.5.em,
+    placeholderVerticalAlign = PlaceholderVerticalAlign.TextCenter
+)
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @Composable
@@ -92,16 +91,13 @@ fun SettingsDialog(
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center
             ) {
                 if (uiState.showLoading) {
                     LoadingRow(title = stringResource(R.string.fetching_languages))
                 } else {
-                    val transformation = remember { SizeTransformation(percent = RESIZE_PERCENT) }
-                    CompositionLocalProvider(LocalSizeTransformation provides transformation) {
-                        LanguageRow(uiState.languages, selectedLanguage.value, onLanguageSelected)
-                    }
-                    Spacer(modifier = Modifier.height(16.dp))
+                    LanguageRow(uiState.languages, selectedLanguage.value, onLanguageSelected)
                 }
             }
         }
@@ -123,6 +119,7 @@ private fun LanguageRow(
         Text(stringResource(R.string.language))
         DropdownMenu(
             expanded = showDropdown,
+            modifier = Modifier.fillMaxHeight(0.4f),
             onDismissRequest = { showDropdown = false }
         ) {
             languages.forEach { language ->
@@ -146,7 +143,7 @@ private fun ToggleContent(countryName: String, flagUrl: String, onClick: () -> U
     Text(
         text = buildAnnotatedString {
             appendInlineContent(FLAG_ID)
-            append(countryName)
+            append("  $countryName")
             appendInlineContent(DROPDOWN_ID)
         },
         inlineContent = mapOf(arrowContent, flagContent),
@@ -163,7 +160,7 @@ private fun DropdownItem(countryName: String, flagUrl: String, selected: Boolean
                     appendInlineContent(TICK_ID)
                 }
                 appendInlineContent(FLAG_ID)
-                append(countryName)
+                append("  $countryName")
             },
             inlineContent = inlineContent(flagUrl, countryName, selected)
         )
@@ -176,11 +173,7 @@ private fun inlineContent(flagUrl: String, countryName: String, selected: Boolea
 }
 
 private fun iconContent(id: String, icon: ImageVector) = id to InlineTextContent(
-    placeholder = Placeholder(
-        width = 2.em,
-        height = 1.em,
-        placeholderVerticalAlign = PlaceholderVerticalAlign.TextCenter
-    ),
+    placeholder = placeholder,
     children = {
         Image(
             imageVector = icon,
@@ -191,26 +184,13 @@ private fun iconContent(id: String, icon: ImageVector) = id to InlineTextContent
 )
 
 private fun flagContent(flagUrl: String, countryName: String) = FLAG_ID to InlineTextContent(
-    placeholder = Placeholder(
-        width = 2.em,
-        height = 1.em,
-        placeholderVerticalAlign = PlaceholderVerticalAlign.TextCenter
-    ),
+    placeholder = placeholder,
     children = {
-        val sizeTransformation = LocalSizeTransformation.current
-        /**
-         * We resize country flag bitmap by [RESIZE_PERCENT] percent to improve render performance.
-         * Then we set contentScale as [FixedScale] with 100/[RESIZE_PERCENT] value to match the original size.
-         * Why?: Dropdown menu does not use LazyColumn and we are rendering all of the country flags at once :(
-         */
         Image(
-            painter = rememberImagePainter(
-                data = flagUrl,
-                builder = { transformations(sizeTransformation) },
-            ),
-            contentDescription = stringResource(id = R.string.flag_content_description, countryName),
-            modifier = Modifier.padding(end = 8.dp),
-            contentScale = FixedScale(100f / RESIZE_PERCENT),
+            painter = rememberImagePainter(data = flagUrl),
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize(),
+            contentDescription = stringResource(id = R.string.flag_content_description, countryName)
         )
     }
 )
