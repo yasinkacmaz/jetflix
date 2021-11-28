d6c2af43d7e84b9fb3368def241d81f97562d3b7
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2021-11-28 17:53:45 +0300

    Make movie grid loading and error columns spannable

--- app/src/main/java/com/yasinkacmaz/jetflix/ui/common/loading/LoadingRow.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/ui/common/loading/LoadingRow.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,20 +14,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun LoadingRow(title: String) {
+fun LoadingRow(title: String, modifier: Modifier = Modifier) {
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
-            .wrapContentHeight()
             .padding(8.dp),
-        horizontalArrangement = Arrangement.Center,
+        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        CircularProgressIndicator(
-            modifier = Modifier
-                .size(40.dp)
-                .padding(end = 8.dp)
-        )
+        CircularProgressIndicator(modifier = Modifier.size(40.dp))
         Text(title)
     }
 }
--- app/src/main/java/com/yasinkacmaz/jetflix/ui/movies/MoviesGrid.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/ui/movies/MoviesGrid.kt
@@ -1,16 +1,20 @@
 package com.yasinkacmaz.jetflix.ui.movies
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.GridItemSpan
+import androidx.compose.foundation.lazy.LazyGridItemSpanScope
 import androidx.compose.foundation.lazy.LazyGridScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -29,14 +33,25 @@ import com.yasinkacmaz.jetflix.ui.movies.movie.Movie
 import com.yasinkacmaz.jetflix.ui.movies.movie.MovieContent
 import com.yasinkacmaz.jetflix.ui.navigation.LocalNavigator
 import com.yasinkacmaz.jetflix.ui.navigation.Screen.MovieDetail
+import com.yasinkacmaz.jetflix.util.toDp
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+
+private const val CELL_COUNT = 2
+private val GRID_SPACING = 8.dp
+
+@OptIn(ExperimentalFoundationApi::class)
+private val span: (LazyGridItemSpanScope) -> GridItemSpan = { GridItemSpan(CELL_COUNT) }
 
 @Composable
 fun MoviesGrid() {
     val moviesViewModel = viewModel<MoviesViewModel>()
     val movies = moviesViewModel.movies.collectAsLazyPagingItems()
     val state = rememberLazyListState()
+    val navigator = LocalNavigator.current
+    val onMovieClicked: (Int) -> Unit = { movieId ->
+        navigator.navigateTo(MovieDetail(movieId))
+    }
     LaunchedEffect(Unit) {
         moviesViewModel.filterStateChanges
             .onEach {
@@ -46,25 +61,35 @@ fun MoviesGrid() {
             .launchIn(this)
     }
 
-    LazyMoviesGrid(movies, state)
+    when (movies.loadState.refresh) {
+        is LoadState.Loading -> {
+            LoadingColumn(stringResource(id = R.string.fetching_movies))
+        }
+        is LoadState.Error -> {
+            val error = movies.loadState.refresh as LoadState.Error
+            ErrorColumn(error.error.message.orEmpty())
+        }
+        else -> {
+            LazyMoviesGrid(state, movies, onMovieClicked)
+        }
+    }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
+@OptIn(ExperimentalFoundationApi::class)
 private fun LazyMoviesGrid(
+    state: LazyListState,
     moviePagingItems: LazyPagingItems<Movie>,
-    state: LazyListState
+    onMovieClicked: (Int) -> Unit
 ) {
-    val navigator = LocalNavigator.current
-    val onMovieClicked: (Int) -> Unit = { movieId ->
-        navigator.navigateTo(MovieDetail(movieId))
-    }
     LazyVerticalGrid(
-        cells = GridCells.Fixed(2),
+        cells = GridCells.Fixed(CELL_COUNT),
         contentPadding = PaddingValues(
-            start = 8.dp,
-            bottom = LocalWindowInsets.current.navigationBars.top.dp
+            start = GRID_SPACING,
+            end = GRID_SPACING,
+            bottom = LocalWindowInsets.current.navigationBars.bottom.toDp().dp.plus(GRID_SPACING)
         ),
+        horizontalArrangement = Arrangement.spacedBy(GRID_SPACING, Alignment.CenterHorizontally),
         state = state,
         content = {
             items(moviePagingItems.itemCount) { index ->
@@ -73,14 +98,10 @@ private fun LazyMoviesGrid(
                     movie,
                     Modifier
                         .height(320.dp)
-                        .padding(end = 8.dp)
-                        .padding(vertical = 8.dp),
+                        .padding(vertical = GRID_SPACING),
                     onMovieClicked
                 )
             }
-
-            // TODO: LazyVerticalGrid does not have span strategy.
-            //  Find a way to display loading and error items at full width or size.
             renderLoading(moviePagingItems.loadState)
             renderError(moviePagingItems.loadState)
         }
@@ -89,40 +110,19 @@ private fun LazyMoviesGrid(
 
 @OptIn(ExperimentalFoundationApi::class)
 private fun LazyGridScope.renderLoading(loadState: CombinedLoadStates) {
-    when {
-        loadState.refresh is LoadState.Loading -> {
-            item {
-                val title = stringResource(id = R.string.fetching_movies)
-                // TODO: Find a way to fill max size
-                LoadingColumn(title)
-            }
-        }
-        loadState.append is LoadState.Loading -> {
-            item {
-                val title = stringResource(R.string.fetching_more_movies)
-                // TODO: Find a way to fill max width
-                LoadingRow(title = title)
-            }
-        }
+    if (loadState.append !is LoadState.Loading) return
+
+    item(span = span) {
+        val title = stringResource(R.string.fetching_more_movies)
+        LoadingRow(title = title, modifier = Modifier.padding(vertical = GRID_SPACING))
     }
 }
 
 @OptIn(ExperimentalFoundationApi::class)
 private fun LazyGridScope.renderError(loadState: CombinedLoadStates) {
-    when {
-        loadState.refresh is LoadState.Error -> {
-            val error = loadState.refresh as LoadState.Error
-            item {
-                // TODO: Find a way to fill max size
-                ErrorColumn(error.error.message.orEmpty())
-            }
-        }
-        loadState.append is LoadState.Error -> {
-            val error = loadState.append as LoadState.Error
-            item {
-                // TODO: Find a way to fill max width
-                ErrorRow(title = error.error.message.orEmpty())
-            }
-        }
+    val message = (loadState.append as? LoadState.Error)?.error?.message ?: return
+
+    item(span = span) {
+        ErrorRow(title = message, modifier = Modifier.padding(vertical = GRID_SPACING))
     }
 }
