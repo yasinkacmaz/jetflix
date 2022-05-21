eebeb9ca5492b28e8e199b5389c79179ce3dbed2
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-05-21 17:48:06 +0300

    Bump compose version to 1.2.0-beta02

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,8 +2,8 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val minSdk = 21
-    const val targetSdk = 31
-    const val compileSdk = "android-Tiramisu"
+    const val targetSdk = 32
+    const val compileSdk = 32
     const val versionCode = 3
     const val versionName = "1.2.0"
     val javaVersion = JavaVersion.VERSION_11
