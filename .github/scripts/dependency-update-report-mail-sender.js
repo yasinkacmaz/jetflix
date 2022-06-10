498fc56153c98f4305f4e5b6f030b3cfa3b1ced2
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-06-10 22:54:39 +0300

    Fix dependency update report mail sender connection

--- .github/scripts/dependency-update-report-mail-sender.js
+++ .github/scripts/dependency-update-report-mail-sender.js
@@ -3,15 +3,10 @@ module.exports = ({ }) => {
     execSync(`npm install nodemailer`)
     const nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
-        host: "smtp.live.com",
-        port: 587,
-        secureConnection: false,
+        service: "Hotmail",
         auth: {
             user: `${process.env.MAIL_USERNAME}`,
             pass: `${process.env.MAIL_PASSWORD}`
-        },
-        tls: {
-            ciphers: 'SSLv3'
         }
     });
     const report = require('fs').readFileSync('build/dependencyUpdates/dependency_update_report.txt', 'utf8')
