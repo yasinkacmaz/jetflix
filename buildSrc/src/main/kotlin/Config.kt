190381aa292aa60a79fb4d7af801509fe5be71e6
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-06-19 18:21:32 +0300

    Bump version to 1.3.0

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -4,7 +4,7 @@ object Config {
     const val minSdk = 21
     const val targetSdk = 33
     const val compileSdk = 33
-    const val versionCode = 3
-    const val versionName = "1.2.0"
+    const val versionCode = 4
+    const val versionName = "1.3.0"
     val javaVersion = JavaVersion.VERSION_11
 }
