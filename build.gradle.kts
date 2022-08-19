9e991055a2faa0506de64153d9d979d9c9cd658b
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-08-20 00:20:08 +0300

    Bump gradle version to 7.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,7 +75,7 @@ tasks.withType<Test>().configureEach {
 // Change gradleVersion and run gradlew wrapper to properly update gradle wrapper
 tasks.named<Wrapper>("wrapper") {
     distributionType = Wrapper.DistributionType.BIN
-    gradleVersion = "7.5-rc-1"
+    gradleVersion = "7.5.1"
 }
 
 task("clean", Delete::class) {
