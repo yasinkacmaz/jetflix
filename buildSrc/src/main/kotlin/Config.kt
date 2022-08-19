40d1d74de9977b49f7c9d4a8e35cd77ae4594201
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-08-20 00:38:36 +0300

    Set target and compile sdk to 33
    Android 13 aka. Tiramisu

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -2,8 +2,8 @@ import org.gradle.api.JavaVersion
 
 object Config {
     const val minSdk = 21
-    const val targetSdk = 32
-    const val compileSdk = 32
+    const val targetSdk = 33
+    const val compileSdk = 33
     const val versionCode = 3
     const val versionName = "1.2.0"
     val javaVersion = JavaVersion.VERSION_11
