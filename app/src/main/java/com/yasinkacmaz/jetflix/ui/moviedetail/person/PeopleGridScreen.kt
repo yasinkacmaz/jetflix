4bd98e8c4a421279678eb9573429b1c53b030fd6
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-08-21 02:57:31 +0300

    Fix people grid screen item animation

--- app/src/main/java/com/yasinkacmaz/jetflix/ui/moviedetail/person/PeopleGridScreen.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/ui/moviedetail/person/PeopleGridScreen.kt
@@ -16,6 +16,11 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
@@ -25,6 +30,7 @@ import com.yasinkacmaz.jetflix.ui.moviedetail.credits.Person
 import com.yasinkacmaz.jetflix.util.animation.ItemAnimationArgs
 import com.yasinkacmaz.jetflix.util.animation.animateItem
 import com.yasinkacmaz.jetflix.util.toDp
+import kotlin.math.absoluteValue
 
 @Composable
 fun PeopleGridScreen(people: List<Person>) {
@@ -77,21 +83,40 @@ private fun LazyGridScope.peopleGridContent(
 
 @Composable
 private fun LazyGridState.calculateDelayAndEasing(index: Int, columnCount: Int): Pair<Int, Easing> {
-    val rowIndex = index / columnCount
-    val visibleRows = layoutInfo.visibleItemsInfo.count()
-    val firstVisibleRowIndex = firstVisibleItemIndex
-    val firstVisibleRow = firstVisibleRowIndex + 1
-    val scrollingToBottom = firstVisibleRowIndex <= rowIndex
-    val isFirstLoad = visibleRows == 0
-    val rowMultiplier = when {
-        isFirstLoad -> rowIndex
-        scrollingToBottom -> firstVisibleRowIndex + visibleRows - rowIndex
-        else -> firstVisibleRow - rowIndex // scrolling to top
+    val visibleItems = layoutInfo.visibleItemsInfo
+    val scrollState = when {
+        visibleItems.isEmpty() -> ScrollState.INITIAL
+        isScrollingUp() -> ScrollState.UP
+        else -> ScrollState.DOWN
     }
-    val rowDelay = 200 * rowMultiplier
-    val scrollDirectionMultiplier = if (isFirstLoad || scrollingToBottom) 1 else -1
-    val column = (index % columnCount) + 1
-    val columnDelay = column * 150 * scrollDirectionMultiplier
-    val easing = if (isFirstLoad || scrollingToBottom) LinearOutSlowInEasing else FastOutSlowInEasing
+    val rowDelay = when (scrollState) {
+        ScrollState.INITIAL -> index / columnCount
+        ScrollState.UP -> (visibleItems.last().index - index).absoluteValue % (visibleItems.count() / columnCount)
+        ScrollState.DOWN -> (index - visibleItems.first().index).absoluteValue % (visibleItems.count() / columnCount)
+    } * 250
+    val scrollDirectionMultiplier = if (scrollState == ScrollState.UP) 1 else -1
+    val columnDelay = (columnCount - index % columnCount) * 150 * scrollDirectionMultiplier
+    val easing = if (scrollState == ScrollState.UP) LinearOutSlowInEasing else FastOutSlowInEasing
     return rowDelay + columnDelay to easing
+}
+
+enum class ScrollState { INITIAL, UP, DOWN }
+
+// Taken from: https://stackoverflow.com/a/68731559
+@Composable
+private fun LazyGridState.isScrollingUp(): Boolean {
+    var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
+    var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
+    return remember(this) {
+        derivedStateOf {
+            if (previousIndex != firstVisibleItemIndex) {
+                previousIndex > firstVisibleItemIndex
+            } else {
+                previousScrollOffset >= firstVisibleItemScrollOffset
+            }.also {
+                previousIndex = firstVisibleItemIndex
+                previousScrollOffset = firstVisibleItemScrollOffset
+            }
+        }
+    }.value
 }
