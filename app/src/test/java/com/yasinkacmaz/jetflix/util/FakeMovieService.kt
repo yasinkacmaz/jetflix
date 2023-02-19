09cc6db63133699253874a7728e20edf28bed624
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2023-02-20 00:01:03 +0300

    Use fakes when testing MoviesPagingSourceTest

--- app/src/test/java/com/yasinkacmaz/jetflix/util/FakeMovieService.kt
+++ app/src/test/java/com/yasinkacmaz/jetflix/util/FakeMovieService.kt
@@ -5,6 +5,7 @@ import com.yasinkacmaz.jetflix.data.Genre
 import com.yasinkacmaz.jetflix.data.GenresResponse
 import com.yasinkacmaz.jetflix.data.ImagesResponse
 import com.yasinkacmaz.jetflix.data.MovieDetailResponse
+import com.yasinkacmaz.jetflix.data.MovieResponse
 import com.yasinkacmaz.jetflix.data.MoviesResponse
 import com.yasinkacmaz.jetflix.service.MovieService
 
@@ -15,13 +16,15 @@ class FakeMovieService : MovieService {
     val movieDetailResponse: MovieDetailResponse = parseJson("movie_detail.json")
     val creditsResponse: CreditsResponse = parseJson("credits.json")
     val imagesResponse: ImagesResponse = parseJson("images.json")
+    val moviesResponse = MoviesResponse(1, listOf(MovieResponse(1, "movie", "", "", "", "", "", 1.1, 1)), 1, 1)
+    val searchResponse = MoviesResponse(1, listOf(MovieResponse(1, "search", "", "", "", "", "", 1.1, 1)), 1, 1)
 
     override suspend fun fetchMovies(pageNumber: Int, options: Map<String, String>): MoviesResponse {
-        TODO("Not yet implemented")
+        return moviesResponse
     }
 
     override suspend fun search(pageNumber: Int, searchQuery: String, includeAdult: Boolean): MoviesResponse {
-        TODO("Not yet implemented")
+        return searchResponse
     }
 
     override suspend fun fetchGenres(): GenresResponse {
