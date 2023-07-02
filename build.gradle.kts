436b1ea17afc8fdaad790c1cb07188496946d4b9
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2023-07-02 19:15:12 +0300

    Bump gradle version to 8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ tasks.withType<Test>().configureEach {
 // Change gradleVersion and run gradlew wrapper to properly update gradle wrapper
 tasks.named<Wrapper>("wrapper") {
     distributionType = Wrapper.DistributionType.BIN
-    gradleVersion = "8.0-rc-2"
+    gradleVersion = "8.2"
 }
 
 task("clean", Delete::class) {
