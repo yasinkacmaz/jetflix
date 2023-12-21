9140516896c725a9188c02719bdd7a41b55176f2
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2023-12-21 14:53:51 +0300

    Bump dependency updates plugin to 0.50.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.serialization) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.versions) apply true
+    alias(libs.plugins.dependencyVersions) apply true
 }
 
 subprojects {
