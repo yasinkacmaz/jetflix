995708e02fe09ec3569cd44226c8a6619c3190e3
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2024-09-10 19:22:55 +0300

    Bump gradle version to 8.10.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,5 +88,5 @@ tasks.withType<Test>().configureEach {
 // Change gradleVersion and run gradlew wrapper to properly update gradle wrapper
 tasks.named<Wrapper>("wrapper") {
     distributionType = Wrapper.DistributionType.BIN
-    gradleVersion = "8.10"
+    gradleVersion = "8.10.1"
 }
