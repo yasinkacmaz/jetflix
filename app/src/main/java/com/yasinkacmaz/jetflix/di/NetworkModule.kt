6ebc3bced3348ebf7f7a88a84a21270288a4a34f
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2024-09-12 00:09:19 +0300

    Use ktor default request instead of interceptors

--- app/src/main/java/com/yasinkacmaz/jetflix/di/NetworkModule.kt
+++ app/src/main/java/com/yasinkacmaz/jetflix/di/NetworkModule.kt
@@ -2,41 +2,41 @@ package com.yasinkacmaz.jetflix.di
 
 import android.content.Context
 import com.yasinkacmaz.jetflix.R
-import com.yasinkacmaz.jetflix.util.interceptor.ApiKeyInterceptor
-import com.yasinkacmaz.jetflix.util.interceptor.LanguageInterceptor
+import com.yasinkacmaz.jetflix.ui.settings.LanguageDataStore
 import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.serialization.kotlinx.json.json
-import okhttp3.Interceptor
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import org.koin.dsl.module
 
 val networkModule = module {
-    single<Set<Interceptor>> {
-        setOf(
-            ApiKeyInterceptor(get<Context>().getString(R.string.api_key)),
-            LanguageInterceptor(get()),
-        )
-    }
-
-    single<HttpClientEngine> {
-        OkHttp.create {
-            config {
-                interceptors().addAll(get<Set<Interceptor>>())
-            }
-        }
-    }
+    single<HttpClientEngine> { OkHttp.create() }
 
     single<HttpClient> {
         HttpClient(get<HttpClientEngine>()) {
             install(ContentNegotiation) {
                 json(get())
             }
-            defaultRequest {
-                url("https://api.themoviedb.org/3/")
-            }
+            defaultRequest { url("https://api.themoviedb.org/3/") }
+            defaultApiKeyParameter(get<Context>().getString(R.string.api_key))
+            defaultLanguageParameter(get())
         }
+    }
+}
+
+fun HttpClientConfig<*>.defaultApiKeyParameter(apiKey: String) = defaultRequest {
+    url {
+        parameters.append("api_key", apiKey)
+    }
+}
+
+fun HttpClientConfig<*>.defaultLanguageParameter(languageDataStore: LanguageDataStore) = defaultRequest {
+    url {
+        parameters.append("language", runBlocking { languageDataStore.languageCode.first() })
     }
 }
