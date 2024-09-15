04892f725266e8a7aebb7842a60e6efa3bc04fa1
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2024-09-15 17:56:21 +0300

    Update test dependencies

--- app/src/test/java/com/yasinkacmaz/jetflix/ui/movies/MoviesPagingSourceTest.kt
+++ app/src/test/java/com/yasinkacmaz/jetflix/ui/movies/MoviesPagingSourceTest.kt
@@ -6,14 +6,12 @@ import com.yasinkacmaz.jetflix.ui.filter.MovieRequestOptionsMapper
 import com.yasinkacmaz.jetflix.ui.movies.movie.MovieMapper
 import com.yasinkacmaz.jetflix.util.CoroutineTestRule
 import com.yasinkacmaz.jetflix.util.client.FakeMovieClient
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
-@ExperimentalCoroutinesApi
 class MoviesPagingSourceTest {
     @get:Rule
     val coroutineTestRule = CoroutineTestRule()
@@ -25,7 +23,7 @@ class MoviesPagingSourceTest {
     private val filterState = FilterState()
     private val loadParams = PagingSource.LoadParams.Refresh(1, 1, true)
 
-    lateinit var moviesPagingSource: MoviesPagingSource
+    private lateinit var moviesPagingSource: MoviesPagingSource
 
     @Test
     fun `should call movies endpoint when query is empty`() = runTest {
--- app/src/test/java/com/yasinkacmaz/jetflix/util/FlowTest.kt
+++ app/src/test/java/com/yasinkacmaz/jetflix/util/FlowTest.kt
@@ -1,14 +1,10 @@
 package com.yasinkacmaz.jetflix.util
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.test.TestScope
 
-context(TestScope)
-@OptIn(ExperimentalCoroutinesApi::class)
 fun <T> Flow<T>.test(): List<T> {
     val values = mutableListOf<T>()
     this
