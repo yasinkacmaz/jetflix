a6dd549da6f267be103cfeb2364236bab0dde58b
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2024-09-16 01:39:48 +0300

    Fix movies screen appbar

---
+++ app/src/main/kotlin/com/yasinkacmaz/jetflix/ui/theme/Spacing.kt
@@ -0,0 +1,24 @@
+package com.yasinkacmaz.jetflix.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Stable
+data object Spacing {
+    val xs: Dp = 4.dp
+    val s: Dp = 8.dp
+    val m: Dp = 12.dp
+    val l: Dp = 16.dp
+    val xl: Dp = 24.dp
+    val xxl: Dp = 32.dp
+}
+
+val LocalSpacing = staticCompositionLocalOf { Spacing }
+
+val MaterialTheme.spacing: Spacing
+    @Composable
+    get() = LocalSpacing.current
