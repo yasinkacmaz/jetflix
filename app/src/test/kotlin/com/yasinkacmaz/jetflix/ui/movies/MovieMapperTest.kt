6e94504fde88762eb092a887324e14c8d53fa0ce
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2024-09-15 23:41:19 +0300

    Map movie vote average with single decimal point

--- app/src/test/kotlin/com/yasinkacmaz/jetflix/ui/movies/MovieMapperTest.kt
+++ app/src/test/kotlin/com/yasinkacmaz/jetflix/ui/movies/MovieMapperTest.kt
@@ -7,10 +7,10 @@ import io.kotest.matchers.shouldBe
 import org.junit.Test
 
 class MovieMapperTest {
+    private val movieResponse = MovieResponse(1, "Date", "Name", "Title", "Language", "Overview", "Poster", 1.1, 1)
+
     @Test
     fun map() {
-        val movieResponse = MovieResponse(1, "Date", "Name", "Title", "Language", "Overview", "Poster", 1.1, 1)
-
         val movie = MovieMapper().map(movieResponse)
 
         movie.id shouldBe movieResponse.id
@@ -19,5 +19,12 @@ class MovieMapperTest {
         movie.posterPath shouldBe movieResponse.posterPath.orEmpty().toPosterUrl()
         movie.voteAverage shouldBe movieResponse.voteAverage
         movie.voteCount shouldBe movieResponse.voteCount
+    }
+
+    @Test
+    fun `Should map vote average with single decimal point`() {
+        val movie = MovieMapper().map(movieResponse.copy(voteAverage = 1.234))
+
+        movie.voteAverage shouldBe 1.2
     }
 }
