ccbbd33f23c8fccc774678b730ea4f0c5b222f19
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2024-09-17 03:08:53 +0300

    Fix filter bottom sheet ui

--- app/src/main/kotlin/com/yasinkacmaz/jetflix/ui/filter/option/GenresOption.kt
+++ app/src/main/kotlin/com/yasinkacmaz/jetflix/ui/filter/option/GenresOption.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
@@ -28,12 +27,12 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.yasinkacmaz.jetflix.R
 import com.yasinkacmaz.jetflix.ui.filter.FilterSectionDivider
 import com.yasinkacmaz.jetflix.ui.filter.FilterSectionTitle
 import com.yasinkacmaz.jetflix.ui.filter.FilterState
 import com.yasinkacmaz.jetflix.ui.filter.genres.GenreUiModel
+import com.yasinkacmaz.jetflix.ui.theme.spacing
 
 typealias GenresFilterOption = Pair<List<GenreUiModel>, MutableList<Int>>
 
@@ -50,9 +49,9 @@ class GenresOption(override val defaultValue: GenresFilterOption) : FilterOption
         val (genreUiModels, selectedGenreIds) = currentValue
         FilterSectionTitle(painter = rememberVectorPainter(image = Icons.Default.Category), title = R.string.genres)
         FlowRow(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(horizontal = MaterialTheme.spacing.l, vertical = MaterialTheme.spacing.xs),
+            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.m),
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.s),
         ) {
             genreUiModels.forEach { genreUiModel ->
                 val genreId = genreUiModel.genre.id
@@ -87,7 +86,7 @@ class GenresOption(override val defaultValue: GenresFilterOption) : FilterOption
             .scale(scale)
             .shadow(animateDpAsState(if (selected) 8.dp else 4.dp, label = "GenreShadow").value, shape)
             .background(MaterialTheme.colorScheme.surface)
-            .border(2.dp, Brush.horizontalGradient(colors), shape)
+            .border(1.5.dp, Brush.horizontalGradient(colors), shape)
             .background(Brush.horizontalGradient(animatedColors), shape)
             .selectable(
                 selected,
@@ -96,13 +95,13 @@ class GenresOption(override val defaultValue: GenresFilterOption) : FilterOption
                     onClicked(selected)
                 },
             )
-            .padding(horizontal = 10.dp, vertical = 3.dp)
+            .padding(horizontal = MaterialTheme.spacing.s, vertical = MaterialTheme.spacing.xs)
 
         Text(
             text = uiModel.genre.name.orEmpty(),
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
-            style = MaterialTheme.typography.bodyMedium.copy(fontSize = 17.sp),
+            style = MaterialTheme.typography.bodyLarge,
             modifier = modifier,
         )
     }
