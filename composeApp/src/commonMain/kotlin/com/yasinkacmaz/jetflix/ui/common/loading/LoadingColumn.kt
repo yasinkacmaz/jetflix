a83fcf8a8daac49aea48635d4b6590ae730b6719
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2025-03-23 19:27:53 +0700

    Remove previews
    For now this is required since we cannot use previews with compose multiplatform
    In the future releases will look at possible ways to use previews on UI tests

--- composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/ui/common/loading/LoadingColumn.kt
+++ composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/ui/common/loading/LoadingColumn.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yasinkacmaz.jetflix.ui.theme.spacing
 
@@ -32,10 +31,4 @@ fun LoadingColumn(title: String, modifier: Modifier = Modifier) {
             )
         }
     }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-private fun LoadingColumnPreview() {
-    LoadingColumn(title = "Please wait...")
 }
