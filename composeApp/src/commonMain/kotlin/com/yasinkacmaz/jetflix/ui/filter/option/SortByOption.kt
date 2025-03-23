898b1aedd420a9d85b6e54b0ba61cd2cb98a63fe
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2025-03-23 17:53:57 +0700

    Use compose multiplatform resources

--- composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/ui/filter/option/SortByOption.kt
+++ composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/ui/filter/option/SortByOption.kt
@@ -1,20 +1,27 @@
 package com.yasinkacmaz.jetflix.ui.filter.option
 
-import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Sort
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.res.stringResource
-import com.yasinkacmaz.jetflix.R
 import com.yasinkacmaz.jetflix.ui.filter.FilterGrid
 import com.yasinkacmaz.jetflix.ui.filter.FilterRadioItem
 import com.yasinkacmaz.jetflix.ui.filter.FilterSectionDivider
 import com.yasinkacmaz.jetflix.ui.filter.FilterSectionTitle
 import com.yasinkacmaz.jetflix.ui.filter.FilterState
+import jetflix.composeapp.generated.resources.Res
+import jetflix.composeapp.generated.resources.sort_by
+import jetflix.composeapp.generated.resources.sort_by_original_title
+import jetflix.composeapp.generated.resources.sort_by_popularity
+import jetflix.composeapp.generated.resources.sort_by_release_date
+import jetflix.composeapp.generated.resources.sort_by_revenue
+import jetflix.composeapp.generated.resources.sort_by_vote_average
+import jetflix.composeapp.generated.resources.sort_by_vote_count
 import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.stringResource
 
 data class SortByOption(override val defaultValue: SortBy) : FilterOption<SortBy> {
     override var currentValue: SortBy = defaultValue
@@ -26,12 +33,12 @@ data class SortByOption(override val defaultValue: SortBy) : FilterOption<SortBy
         val sortByState = remember(defaultValue) { mutableStateOf(currentValue) }
         FilterSectionTitle(
             painter = rememberVectorPainter(image = Icons.AutoMirrored.Default.Sort),
-            title = R.string.sort_by,
+            title = Res.string.sort_by,
         )
         val sortByValues = SortBy.entries
         FilterGrid(sortByValues) { sortBy ->
             val selected = sortByState.value == sortBy
-            FilterRadioItem(title = stringResource(id = sortBy.titleResId), selected = selected) {
+            FilterRadioItem(title = stringResource(sortBy.title), selected = selected) {
                 currentValue = sortBy
                 sortByState.value = sortBy
                 onChanged()
@@ -42,11 +49,11 @@ data class SortByOption(override val defaultValue: SortBy) : FilterOption<SortBy
 }
 
 @Serializable
-enum class SortBy(@StringRes val titleResId: Int, val by: String) {
-    POPULARITY(R.string.sort_by_popularity, "popularity"),
-    VOTE_COUNT(R.string.sort_by_vote_count, "vote_count"),
-    VOTE_AVERAGE(R.string.sort_by_vote_average, "vote_average"),
-    RELEASE_DATE(R.string.sort_by_release_date, "release_date"),
-    ORIGINAL_TITLE(R.string.sort_by_original_title, "original_title"),
-    REVENUE(R.string.sort_by_revenue, "revenue"),
+enum class SortBy(val title: StringResource, val by: String) {
+    POPULARITY(Res.string.sort_by_popularity, "popularity"),
+    VOTE_COUNT(Res.string.sort_by_vote_count, "vote_count"),
+    VOTE_AVERAGE(Res.string.sort_by_vote_average, "vote_average"),
+    RELEASE_DATE(Res.string.sort_by_release_date, "release_date"),
+    ORIGINAL_TITLE(Res.string.sort_by_original_title, "original_title"),
+    REVENUE(Res.string.sort_by_revenue, "revenue"),
 }
