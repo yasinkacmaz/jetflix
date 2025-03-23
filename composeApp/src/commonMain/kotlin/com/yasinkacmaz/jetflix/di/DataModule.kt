9073976de5abb45cd8109f84e7d9746d4bb0f32e
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2025-03-23 19:21:52 +0700

    Create local datastore for all platforms

--- composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/di/DataModule.kt
@@ -1,9 +1,9 @@
 package com.yasinkacmaz.jetflix.di
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import com.yasinkacmaz.jetflix.data.local.DataStoreFilePathProvider
 import com.yasinkacmaz.jetflix.data.local.LocalDataStore
 import com.yasinkacmaz.jetflix.data.local.PreferencesDataStore
 import com.yasinkacmaz.jetflix.ui.favorites.FavoritesDataStore
@@ -14,13 +14,9 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
-private const val PREFERENCES_FILE_NAME = "jetflix_settings.preferences_pb"
-
 val dataModule = module {
     single<DataStore<Preferences>> {
-        PreferenceDataStoreFactory.createWithPath {
-            get<Context>().filesDir.path.plus("/DataStore/").plus(PREFERENCES_FILE_NAME).toPath()
-        }
+        PreferenceDataStoreFactory.createWithPath { get<DataStoreFilePathProvider>().provide().toPath() }
     }
     singleOf(::PreferencesDataStore).bind(LocalDataStore::class)
     singleOf(::LanguageDataStore)
