253f4759f4ec8d86c4fadaf14cc1465055cfe6a9
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2025-03-23 18:57:54 +0700

    Call koin initializer at platform entry points

--- composeApp/src/desktopMain/kotlin/com/yasinkacmaz/jetflix/main.kt
+++ composeApp/src/desktopMain/kotlin/com/yasinkacmaz/jetflix/main.kt
@@ -2,11 +2,13 @@ package com.yasinkacmaz.jetflix
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.yasinkacmaz.jetflix.di.initializeKoin
 import jetflix.composeapp.generated.resources.Res
 import jetflix.composeapp.generated.resources.app_name
 import org.jetbrains.compose.resources.stringResource
 
 fun main() = application {
+    initializeKoin()
     Window(
         onCloseRequest = ::exitApplication,
         title = stringResource(Res.string.app_name),
