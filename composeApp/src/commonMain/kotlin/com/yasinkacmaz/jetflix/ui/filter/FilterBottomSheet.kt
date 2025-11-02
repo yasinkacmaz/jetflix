0657fca60008a53491f152ec638d00e7c15935c0
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2025-11-03 00:45:03 +0300

    Update compose wasm setup

--- composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/ui/filter/FilterBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/ui/filter/FilterBottomSheet.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -17,6 +18,7 @@ import org.jetbrains.compose.resources.stringResource
 fun FilterBottomSheet(filterState: FilterState?, onDismiss: () -> Unit, onFilterStateChanged: (FilterState) -> Unit) {
     ModalBottomSheet(
         onDismissRequest = onDismiss,
+        sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
         scrimColor = BottomSheetDefaults.ScrimColor.copy(alpha = 0.75f),
         content = {
             if (filterState == null) {
--- composeApp/src/wasmJsMain/kotlin/com/yasinkacmaz/jetflix/Main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/yasinkacmaz/jetflix/Main.kt
@@ -2,22 +2,22 @@ package com.yasinkacmaz.jetflix
 
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.window.CanvasBasedWindow
+import androidx.compose.ui.window.ComposeViewport
 import androidx.navigation.ExperimentalBrowserHistoryApi
-import androidx.navigation.bindToNavigation
+import androidx.navigation.bindToBrowserNavigation
 import androidx.navigation.compose.rememberNavController
 import com.yasinkacmaz.jetflix.di.initializeKoin
 import com.yasinkacmaz.jetflix.ui.navigation.Screen
-import kotlinx.browser.window
+import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalBrowserHistoryApi::class)
 fun main() {
     initializeKoin()
-    CanvasBasedWindow(canvasElementId = "ComposeTarget") {
+    ComposeViewport(document.body!!) {
         val navController = rememberNavController()
         JetflixApp(startScreen = Screen.Splash, navController = navController)
-        LaunchedEffect(Unit) {
-            window.bindToNavigation(navController)
+        LaunchedEffect(navController) {
+            navController.bindToBrowserNavigation()
         }
     }
 }
