bfb9c1ecdc0b9d838f4cb6886a342e63088a9b95
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2025-11-22 02:11:16 +0300

    Use smaller original url at movie detail and images screen (#348)

--- composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/util/String.kt
+++ composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/util/String.kt
@@ -8,11 +8,11 @@ fun String.toSmallPosterUrl() = "https://image.tmdb.org/t/p/w185$this"
 
 fun String.toPosterUrl() = "https://image.tmdb.org/t/p/w342$this"
 
-fun String.toBackdropUrl() = "https://image.tmdb.org/t/p/original$this"
+fun String.toBackdropUrl() = "https://image.tmdb.org/t/p/w780$this"
 
 fun String.toProfilePhotoUrl() = "https://image.tmdb.org/t/p/w185$this"
 
-fun String.toOriginalUrl() = "https://image.tmdb.org/t/p/original$this"
+fun String.toOriginalUrl() = "https://image.tmdb.org/t/p/w780$this"
 
 fun String.toImdbProfileUrl() = "https://www.imdb.com/name/$this"
 
