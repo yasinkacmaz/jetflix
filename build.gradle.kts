cc616dda612459822a8c860363a0d90544be29f0
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2025-11-22 00:40:44 +0300

    Bump gradle version to 9.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,5 +56,5 @@ tasks.withType<Test>().configureEach {
 // Change gradleVersion and run gradlew wrapper to properly update gradle wrapper
 tasks.named<Wrapper>("wrapper") {
     distributionType = Wrapper.DistributionType.BIN
-    gradleVersion = "9.2.0"
+    gradleVersion = "9.2.1"
 }
