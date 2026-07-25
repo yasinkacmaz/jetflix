ae838aa29095d828b715e816911a938b8042f09c
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2026-07-25 17:11:08 +0700

    Sync browser history with navigation

--- composeApp/src/wasmJsMain/kotlin/com/yasinkacmaz/jetflix/Main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/yasinkacmaz/jetflix/Main.kt
@@ -1,23 +1,15 @@
 package com.yasinkacmaz.jetflix
 
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
-import androidx.navigation.ExperimentalBrowserHistoryApi
-import androidx.navigation.bindToBrowserNavigation
-import androidx.navigation.compose.rememberNavController
 import com.yasinkacmaz.jetflix.di.initializeKoin
 import com.yasinkacmaz.jetflix.ui.navigation.Screen
 import kotlinx.browser.document
 
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalBrowserHistoryApi::class)
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     initializeKoin()
     ComposeViewport(document.body!!) {
-        val navController = rememberNavController()
-        JetflixApp(startScreen = Screen.Splash, navController = navController)
-        LaunchedEffect(navController) {
-            navController.bindToBrowserNavigation()
-        }
+        JetflixApp(startScreen = Screen.Splash)
     }
 }
