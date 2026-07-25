904b6efe1c20eba733a7b8ea5dcd2830155cb4d3
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2026-07-26 00:40:06 +0700

    Upgrade project to Java 21

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_17)
+        jvmTarget.set(JvmTarget.JVM_21)
     }
 }
 
@@ -51,8 +51,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     buildFeatures {
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -27,7 +27,7 @@ kotlin {
         }
 
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+            jvmTarget.set(JvmTarget.JVM_21)
             freeCompilerArgs.add("-Xexpect-actual-classes")
         }
     }
