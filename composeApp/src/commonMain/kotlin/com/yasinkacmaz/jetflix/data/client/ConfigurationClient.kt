8dc2ea0ac47cd0b0e9986bc85e8b748a8e1a5ddf
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2026-07-26 13:31:52 +0700

    Deserialize Ktor response body on Default dispatcher
    Ktor response body and error parsing previously blocked the main thread
    Wrapping the logic in Default dispatcher moves deserialization to a CPU thread, preventing UI freezes.

--- composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/data/client/ConfigurationClient.kt
+++ composeApp/src/commonMain/kotlin/com/yasinkacmaz/jetflix/data/client/ConfigurationClient.kt
@@ -2,10 +2,10 @@ package com.yasinkacmaz.jetflix.data.client
 
 import com.yasinkacmaz.jetflix.data.service.ConfigurationService
 import com.yasinkacmaz.jetflix.ui.settings.Language
+import com.yasinkacmaz.jetflix.util.parseBody
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.request.get
 
 class ConfigurationClient(private val httpClient: HttpClient) : ConfigurationService {
-    override suspend fun fetchLanguages(): List<Language> = httpClient.get("configuration/languages").body()
+    override suspend fun fetchLanguages(): List<Language> = httpClient.get("configuration/languages").parseBody()
 }
